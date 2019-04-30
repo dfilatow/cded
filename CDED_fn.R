91ebfba3525e48660367d0a112aa5a5095b90466
dfilatow <32374675+dfilatow@users.noreply.github.com>
2019-04-29 17:12:20 -0700

    Add cded_fn.r
    uploaded cded_fn.r code using github interface

---
+++ CDED_fn.R
@@ -0,0 +1,63 @@
+#########################################################################
+#CDED function
+#input a list of URL addresses for the cded mapsheets you want to download
+#unzip and merge.https://pub.data.gov.bc.ca/datasets/175624
+
+#author: Deepa Filatow
+#modified: April 26, 2019
+#########################################################################
+
+#clear memory
+rm(list = ls())
+
+#load some test inputs
+url1<-"https://pub.data.gov.bc.ca/datasets/175624/93j/093j09_e.dem.zip"
+url2<-"https://pub.data.gov.bc.ca/datasets/175624/93j/093j09_w.dem.zip"
+maplist <- c(url1, url2)
+
+
+#########################################################################
+#define functions
+#########################################################################
+#CDED function
+#input a list of URL addresses for the cded mapsheets you want to download
+#unzip and merge.https://pub.data.gov.bc.ca/datasets/175624
+#########################################################################
+
+cded <- function (maplist){
+  require(raster)
+  require(dplyr)
+  require(rgdal)
+  
+  temp<- tempfile()
+  temp2 <- tempfile()
+  x<-1
+  for (i in maplist) {
+ 
+    download.file(i, temp)
+    unzip(zipfile = temp, exdir = temp2)
+    print (cat("Unzipping ", i))
+    filename <- sub('\\.zip$', '', (sapply (strsplit (i, '/'), tail, 1)))
+    if (x==1){
+      r <- raster(file.path(temp2, filename))
+      print(cat("Loaded ", filename))
+    }
+    else {
+      r<-merge(r, (raster(file.path(temp2, filename))))
+    print(cat("Loaded and merged ", filename))  
+    }
+    
+    x<-x+1
+    }
+  unlink(c(temp, temp2))
+  return (r)
+  
+  }
+##########################################################################
+
+
+
+
+#run function with test data
+cdem93j09 <-cded(maplist)
+plot(cdem93j09)
