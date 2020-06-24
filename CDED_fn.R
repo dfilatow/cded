709522fe376350fad1c30457f088dc93fb6deb67
dfilatow <32374675+dfilatow@users.noreply.github.com>
2020-06-24 14:20:37 -0700

    added create maplist to the function
    Added to the function to allow for the creation of the map list using and aoi. This part of the function uses the bcdata R package to get the 50K mapsheet grid.

--- CDED_fn.R
+++ CDED_fn.R
@@ -7,29 +7,33 @@
 #modified: April 26, 2019
 #########################################################################
 
-#clear memory
-rm(list = ls())
-
-#load some test inputs
-url1<-"https://pub.data.gov.bc.ca/datasets/175624/93j/093j09_e.dem.zip"
-url2<-"https://pub.data.gov.bc.ca/datasets/175624/93j/093j09_w.dem.zip"
-maplist <- c(url1, url2)
-
-
 #########################################################################
-#define functions
+#define cded_get function
+# input an are of interest, aoi to create a list of cded dem url links 
+#unzip and merge files from https://pub.data.gov.bc.ca/datasets/175624
+# return a stitched together dem in the crs provided, crs1
 #########################################################################
 
-#########################################################################
-#cded_get function
-#input a list of URL addresses for the cded mapsheets you want to download
-#unzip and merge.https://pub.data.gov.bc.ca/datasets/175624
-#########################################################################
-
-cded_get <- function (maplist){
+cded_get <- function (crs1 = 3005, aoi){
+  #libraries
   require(raster)
   require(dplyr)
   require(rgdal)
+  require (databc)
+  
+  #url for cded data by east and west map tiles 
+  cdedurl<- "https://pub.data.gov.bc.ca/datasets/175624/"
+  grid50k <- bcdc_query_geodata("https://catalogue.data.gov.bc.ca/dataset/f9483429-fedd-4704-89b9-49fd098d4bdb", crs = crs1)%>%
+    filter( INTERSECTS(aoi)) %>%
+    collect()
+  
+  #add a field and populat with the map block
+  grid50k$block<-substring(grid50k$MAP_TILE, first = 1, last = 4)
+  
+  grid50k$url1<-paste0(cdedurl, grid50k$block,"/", grid50k$MAP_TILE, "_e.dem.zip")
+  grid50k$url2<-paste0(cdedurl, grid50k$block,"/", grid50k$MAP_TILE, "_w.dem.zip")
+  
+  maplist <- c(grid50k$url1, grid50k$url2)
   
   temp<- tempfile()
   temp2 <- tempfile()
@@ -57,9 +61,15 @@ cded_get <- function (maplist){
   }
 ##########################################################################
 
+##########################################################################
+#test function
+##########################################################################
 
+#get a test watershed for the aoi
+aoi <- bcdc_query_geodata("51f20b1a-ab75-42de-809d-bf415a0f9c62", crs = 3005) %>%
+  filter( WATERSHED_GROUP_CODE == "PARS") %>%
+  collect()
 
-
-#run function with test data
-cdem93j09 <-cded_get(maplist)
+#run function with test aoi
+pars_dem <-cded_get(crs = 3005, aoi = aoi)
 plot(cdem93j09)
